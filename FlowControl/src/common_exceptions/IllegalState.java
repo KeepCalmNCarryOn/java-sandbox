d927dc5d38479f95ef59dc2051f19e72f4a3007f
Tonisha <tonishawhyte@yahoo.com>
2012-04-25 13:29:02 -0700

    add IllegalStateException example

--- FlowControl/src/common_exceptions/IllegalState.java
+++ FlowControl/src/common_exceptions/IllegalState.java
@@ -1,20 +1,48 @@
 package common_exceptions;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.Scanner;
+
 /**
  * Examples of IllegalStateException.  
  * 
  * Thrown when the state of the environment doesn't match the operation being
- * attempted.
+ * attempted.  
  * @author Tonisha
  *
  */
 public class IllegalState {
 
 	/**
+	 * Calls methods that throw IllegalStateExceptions.
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		
-
+			writeToClosedStream();
+	}
+	/**
+	 * Attempt to parse a string using a closed Scanner.
+	 */
+	public static void writeToClosedStream(){
+		try{
+			Scanner in = new Scanner(System.in);
+			System.out.println("Enter something: ");
+			if (in.hasNextLine()){
+				String myStr = in.nextLine();
+				if(myStr.contains("something")){
+					System.out.println("You entered something!");
+				}
+				else{
+					System.out.println("You didn't enter \"something\". Haha...");
+				}
+			}
+			in.close();
+			in.nextLine();
+		}catch (IllegalStateException e){
+			e.printStackTrace();
+		}
 	}
 
 }

a7c2fa0e5dc5040b63dc0d26dc97e6378df9fce9
Tonisha Whyte <tonishawhyte@yahoo.com>
2012-05-05 22:23:34 -0700

    changed code to throw ExceptionInInitializerError instead of another exception

--- FlowControl/src/common_exceptions/ExceptionInInstanceInitializer.java
+++ FlowControl/src/common_exceptions/ExceptionInInstanceInitializer.java
@@ -1,29 +1,35 @@
 package common_exceptions;
 /** 
- * Examples of a commonly seen error -the ExceptionInInitializerError.
+ * Examples of a commonly seen exception -the ExceptionInInitializerError.
  * This error occurs when a mistake is made in an initializer.
  * @author Tonisha Whyte
  *
  */
 public class ExceptionInInstanceInitializer {
-	{
-		System.out.println("Inside instance initializer");
-		char[] straws = new char[4];
-		straws[-4] = 'c';
-	}
+	static ExceptionInInstanceInitializer loki = 
+			new ExceptionInInstanceInitializer();
 	
+	/**
+	 * Indicates when the initializer runs.  The print statement will never be
+	 * displayed due to the exception.
+	 */
 	public ExceptionInInstanceInitializer(){
 		System.out.println("Inside constructor after call to super");
 	}
 		
 	/**
-	 * Nothing!
+	 * Calls the constructor.
 	 * Instance initializers are created outside of methods and run right after 
-	 * the call to super() completes.
+	 * the call to super() completes so they can not be caught.
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		new ExceptionInInstanceInitializer();
+		//new ExceptionInInstanceInitializer();
+	}
+	
+	// This initializer accesses an object that is not yet available.
+	{
+		loki.toString();
 	}
 
 }
